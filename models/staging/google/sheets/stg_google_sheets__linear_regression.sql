9f36bd06544b0a25f6cf253b6932d7cb48040f80
Daniel Wilczak <daniel.wilczak@snowflake.com>
2023-08-24 14:09:05 +0000

    Updated project yaml to fix project naming and removed staging type to see if it fixes hex.

--- models/staging/google/sheets/stg_google_sheets__linear_regression.sql
+++ models/staging/google/sheets/stg_google_sheets__linear_regression.sql
@@ -5,10 +5,10 @@ source as ( select * from {{ source('google_sheets', 'linear_regression') }}),
 renamed as (
 
     select
-        _row::INTEGER    as _row,
-        x::INTEGER       as x,
-        y::INTEGER       as y,
-        _fivetran_synced as _fivetran_synced
+        _row,
+        x,
+        y,
+        _fivetran_synced
 
     from source
 
