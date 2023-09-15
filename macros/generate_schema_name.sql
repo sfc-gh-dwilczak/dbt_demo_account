da937ec0d4f54bf080f1f9e32f169cc613e4197e
Daniel Wilczak <daniel.wilczak@snowflake.com>
2023-09-15 19:45:51 +0000

    Updated

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,5 +1,5 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
-    {% if target.name not in ('test', 'Prod') or custom_schema_name is none %}
+    {% if target.name not in ('test', 'production') or custom_schema_name is none %}
         {{ return(target.schema) }}
     {% else %}
         {{ return(custom_schema_name | trim) }}
