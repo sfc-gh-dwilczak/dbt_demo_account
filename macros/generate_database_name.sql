88f99cf88417cbd3089b0d036f6fe25b7c3e1ecf
Jack Nguyen <jackyeenguyen@gmail.com>
2023-09-16 20:01:46 +0000

    merge

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 {% macro generate_database_name(custom_database_name=none, node=none) %}
     {% set prefix = custom_database_name or '' %}
     {% if target.name == 'test' %}
@@ -26,14 +25,3 @@
     {% endif %}
     {{ return(prefix ~ '_' ~ db) }}
 {% endmacro %}
-=======
-{% macro generate_database_name(custom_database_name=none, node=none) %}
-    {% if target.name not in ('test', 'production') or custom_database_name is none %}
-        {{ return(target.database) }}
-    {% else %}
-        {{ return(custom_database_name | trim) }}
-    {% endif %}
-{% endmacro %}
-
-
->>>>>>> fddaa3ea11c4464caf0943229412ec9fb5615c7c
