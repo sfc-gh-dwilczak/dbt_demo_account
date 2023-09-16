2653cfb4c8e94a128d739d8a308bf1a35c89aa59
Jack Nguyen <jackyeenguyen@gmail.com>
2023-09-16 20:00:09 +0000

    updated custom database.schema.alias name

---
+++ macros/generate_database_name.sql
@@ -0,0 +1,27 @@
+{% macro generate_database_name(custom_database_name=none, node=none) %}
+    {% set prefix = custom_database_name or '' %}
+    {% if target.name == 'test' %}
+        {% set prefix = 'dbt_test' %}
+    {% else %}
+        {% set prefix = 'dbt' %}
+    {% endif %}
+    {% if custom_database_name is not none %}
+        {% set prefix = prefix ~ '_' ~ custom_database_name %}
+    {% endif %}
+    {% if target.name not in ('test', 'production') %}
+        {{ return(target.database) }}
+    {% elif node.name.startswith('stg') %}
+        {% set db = 'staging' %}
+    {% elif node.name.startswith('src') %}
+        {% set db = 'sources' %}
+    {% elif node.name.startswith('int') %}
+        {% set db = 'intermediates' %}
+    {% elif node.name.startswith('dim') %}
+        {% set db = 'dimensions' %}
+    {% elif node.name.startswith('fct') %}
+        {% set db = 'facts' %}
+    {% else %}
+        {{ return(prefix) }}
+    {% endif %}
+    {{ return(prefix ~ '_' ~ db) }}
+{% endmacro %}
--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,10 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
-    {% if target.name not in ('test', 'production') or custom_schema_name is none %}
+{% macro generate_schema_name(custom_schema_name=none, node=none) %}
+    {% if target.name not in ('test', 'production') %}
         {{ return(target.schema) }}
-    {% else %}
-        {{ return(custom_schema_name | trim) }}
+    {% elif custom_schema_name is not none %}
+        {{ return(custom_schema_name) }}
     {% endif %}
+    {% set prefix = node.name.split('__')[0] %}
+    {% set schema_split = prefix.split('_')[1:] %}
+    {{ return(schema_split | join('_')) }}
 {% endmacro %}
