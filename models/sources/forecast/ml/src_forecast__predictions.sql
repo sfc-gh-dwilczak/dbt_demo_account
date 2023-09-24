e2ced8054f6b088eacfffa30d9d5a9c199c28d33
Daniel Wilczak <daniel.wilczak@snowflake.com>
2023-09-24 05:45:18 +0000

    Finished model for forecasting.

--- models/sources/forecast/ml/src_forecast__predictions.sql
+++ models/sources/forecast/ml/src_forecast__predictions.sql
@@ -1,69 +1,46 @@
-    /* 
-    < 5 Weekday
-    5 = Saturday
-    6 = Sunday 
-    
-    sudo:
-        if saturday:
-            train model
-            predict and load data
-        else:
-            predict and load data
-*/
+{#
+On the DAG, the model will reference the
+data that the ML model is using to train on.
+#}
+{%- set train -%}
+    CREATE OR REPLACE SNOWFLAKE.ML.FORECAST model2(
+    INPUT_DATA => SYSTEM$REFERENCE('VIEW', '{{ ref('src_forecast__sales_data') }}'),
+    TIMESTAMP_COLNAME => 'date',
+    TARGET_COLNAME => 'sales');
+{%- endset -%}
 
-{% if run_started_at.weekday() == 5 %}
-    {% set train %}
-        CREATE OR REPLACE SNOWFLAKE.ML.FORECAST model2(
-        INPUT_DATA => SYSTEM$REFERENCE('VIEW', '{{ ref('src_forecast__sales_data') }}'),
-        TIMESTAMP_COLNAME => 'date',
-        TARGET_COLNAME => 'sales');
-    {% endset %}
-
-    {% set load %}
-        EXECUTE IMMEDIATE $$
-            BEGIN
-                CALL model2!FORECAST(FORECASTING_PERIODS => 3);
-                LET query_id := SQLID;
-                CREATE OR REPLACE TABLE {{this}} AS
-                     SELECT * FROM TABLE(RESULT_SCAN(:query_id));
-            END;
-        $$;
-    {% endset %}
-    {% set pre_hook = [train,load] %}
-{% else %}
-    {% set load %}
-        EXECUTE IMMEDIATE $$
-            BEGIN
-                CALL model2!FORECAST(FORECASTING_PERIODS => 3);
-                LET query_id := SQLID;
-                CREATE OR REPLACE TABLE {{this}} AS
+{#
+The ML model will be used to forecast data, which
+is saved in a table replacing this staging model.
+#}
+{%- set load -%}
+    EXECUTE IMMEDIATE $$
+        BEGIN
+            CALL model2!FORECAST(FORECASTING_PERIODS => 3);
+            LET query_id := SQLID;
+            CREATE OR REPLACE TABLE {{ this }} AS
                     SELECT * FROM TABLE(RESULT_SCAN(:query_id));
-            END;
-        $$;
-    {% endset %}
-    {% set pre_hook = [load] %}
-{% endif %}
+        END;
+    $$;
+{%- endset -%}
 
-{{ config(pre_hook=pre_hook) }}
+{# The ML model is only re-trained on Saturday. #}
+{%- set is_saturday = run_started_at.weekday() == 5 -%}
 
--- Normal staging model code.
-with
-    source as (
+{%- if is_saturday -%}
+    {%- set post_hook = [train, load] -%}
+{%- else -%}
+    {%- set post_hook = [load] -%}
+{%- endif -%}
 
-        select * from {{ this }}
+{{ config(post_hook=post_hook) }}
 
-    ),
-
-    renamed as (
-
-        select
-            ts,
-            forecast,
-            lower_bound,
-            upper_bound
-
-        from source
-
-    )
-
-select * from renamed
+-- Define table structure for DBT developers to see and DBT auto-complete.
+select
+    null::timestamp     as ts,
+    null::float         as forecast,
+    null::float         as lower_bound,
+    null::float         as upper_bound
+-- The table shouldn't actually have any records.
+where
+    false
