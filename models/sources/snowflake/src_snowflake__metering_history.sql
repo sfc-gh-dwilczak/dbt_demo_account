18af4d258969a1e4dca24c4f1b06a0eb97d3028f
Daniel Wilczak <daniel.wilczak@snowflake.com>
2023-10-05 04:00:54 +0000

    added snowflake database.

---
+++ models/sources/snowflake/src_snowflake__metering_history.sql
@@ -0,0 +1,28 @@
+with 
+
+source as (
+
+    select * from {{ source('snowflake', 'metering_history') }}
+
+),
+
+renamed as (
+
+    select
+        service_type,
+        start_time,
+        end_time,
+        entity_id,
+        name,
+        credits_used_compute,
+        credits_used_cloud_services,
+        credits_used,
+        bytes,
+        files,
+        budget_id
+
+    from source
+
+)
+
+select * from renamed
