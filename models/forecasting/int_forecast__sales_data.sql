c3fc6f86de5e28162f3ad79ca346fe5749211f41
Daniel Wilczak <daniel.wilczak@snowflake.com>
2023-10-06 06:28:05 +0000

    yeet

--- models/forecasting/int_forecast__sales_data.sql
+++ models/forecasting/int_forecast__sales_data.sql
@@ -1,18 +1,3 @@
--- Predict total sales per date for the next 3 days.
-{%- set source_data = "{{ ref('src_forecast__sales_data') }}" -%}
-{%- set source_type = 'view' -%}
-{%- set predict = 'sales' -%}
-{%- set predict_using = 'sold_on' -%}
-{%- set predict_days = 3 -%}
-
-{%- set forecast_hook = forecast_model_hooks(
-    input_data=source_data,
-    input_type=source_type,
-    timestamp_colname=predict_using,
-    target_colname=predict,
-    forecasting_periods=predict_days
-) -%}
-
 
 
 select
