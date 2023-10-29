49480ed4b1ece3f37898448f07f0c06665726ffc
Jack Nguyen <jackyeenguyen@gmail.com>
2023-10-29 23:31:07 +0000

    feat: added support for model versions

--- macros/generate_alias_name.sql
+++ macros/generate_alias_name.sql
@@ -1,9 +1,21 @@
 {% macro generate_alias_name(custom_alias_name, node) %}
-    {% if custom_alias_name is not none %}
-        {{ return(custom_alias_name) }}
-    {% elif target.name == 'production' %}
-        {{ return((node.name.split("__")[0].split("_")[0]) ~ "_" ~ (node.name.split("__")[1:] | join("__"))) }}
+    {% if node.version is none %}
+        {% set suffix = '' %}
     {% else %}
-        {{ return(node.name) }}
+        {% set suffix = '_v' ~ node.version %}
+    {% endif %}
+    {% if custom_alias_name is not none %}
+        {{ return(custom_alias_name ~ suffix) }}
+    {% elif target.name == 'production' %}
+        {{ return(
+            (
+                node.name.split("__")[0].split("_")[0])
+                ~ "_"
+                ~ (node.name.split("__")[1:] | join("__")
+            )
+            ~ suffix
+        ) }}
+    {% else %}
+        {{ return(node.name ~ suffix) }}
     {% endif %}
 {% endmacro %}
