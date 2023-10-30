62dcebde68394ebc76b26f9ce23c89e534ed252e
Jack Nguyen <jackyeenguyen@gmail.com>
2023-10-30 05:33:21 +0000

    fix: edge case for metricflow_time_spine

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,5 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
-    {% if custom_schema_name is not none %}
+    {% if node.name == 'metricflow_time_spine' %}
+        {{ return('semantic_layer') }}
+    {% elif custom_schema_name is not none %}
         {{ return(custom_schema_name) }}
     {% elif target.name == 'production' %}
         {{ return(node.name.split("__")[0].split("_")[1:] | join("_")) }}
