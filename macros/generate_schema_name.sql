a60dcab50948b21789733f5be68d3ba4c86a3b83
Jack Nguyen <jackyeenguyen@gmail.com>
2023-11-01 20:56:41 +0000

    feat: production snapshots go to schema based on name, other environments use a snapshot schema

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,10 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
     {% if node.name == 'metricflow_time_spine' %}
         {{ return('semantic_layer') }}
-    {% elif custom_schema_name is not none %}
+    {% elif
+        custom_schema_name is not none
+        and (custom_schema_name != 'snapshots' or target.name != 'production')
+    %}
         {{ return(custom_schema_name) }}
     {% elif target.name == 'production' %}
         {{ return(node.name.split("__")[0].split("_")[1:] | join("_")) }}
