47dd306fe7fa945c2da05f81dd19c9ca37a02f4e
Jack Nguyen <jackyeenguyen@gmail.com>
2023-11-05 02:10:04 +0000

    nit: simplified with new snapshot macro

--- macros/snapshot/snapshot_with_checksum_key.sql
+++ macros/snapshot/snapshot_with_checksum_key.sql
@@ -1,6 +1,6 @@
 {% macro snapshot_with_checksum_key(model, keys) %}
 select
-    "{{ sorted_columns(model) | join('",\n    "') }}",
+    {{ sorted_select_star(model) }},
     {{ dbt_utils.generate_surrogate_key(
         keys + ['hash(' ~ sorted_select_star(model) ~ ')']
     ) }} as dbt_scd_uk
