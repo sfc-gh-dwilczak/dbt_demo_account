ac3f416376a9cf1917552397b1569cf2a0b7f409
Jack Nguyen <jackyeenguyen@gmail.com>
2023-11-07 02:31:28 +0000

    feat: made it possible to change what the unique key column is called

--- macros/snapshot/generate_snapshot.sql
+++ macros/snapshot/generate_snapshot.sql
@@ -1,15 +1,17 @@
 {% macro generate_snapshot(
     model,
     keys,
+    unique_key_alias='dbt_scd_uk',
     partition_by=none,
     invalidate_hard_deletes=true
 ) %}
-    {% set query = snapshot_with_checksum_key(model, keys) %}
+    {% set query = snapshot_with_checksum_key(model, keys, unique_key_alias) %}
     {% if partition_by is none %}
         {{ return(query) }}
     {% else %}
         {{ return(snapshot_with_partition_by(
             query=query,
+            unique_key=unique_key_alias,
             partition_by=partition_by,
             checksum=hash_agg(model),
             invalidate_hard_deletes=invalidate_hard_deletes
--- macros/snapshot/snapshot_with_checksum_key.sql
+++ macros/snapshot/snapshot_with_checksum_key.sql
@@ -1,9 +1,9 @@
-{% macro snapshot_with_checksum_key(model, keys) %}
+{% macro snapshot_with_checksum_key(model, keys, unique_key_alias) %}
 select
     {{ sorted_select_star(model) }},
     {{ dbt_utils.generate_surrogate_key(
         keys + [hash(model)]
-    ) }} as dbt_scd_uk
+    ) }} as {{ unique_key_alias }}
 from
     {{ model }}
 {% endmacro %}
